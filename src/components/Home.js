2d5b5fecad3c53ddb0940b4e068a18458732b20a
Shalini21csa57 <21csa57@karpagamtech.ac.in>
2024-07-29 23:42:44 +0530

    first

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Home (){
+    return (
+        <div className="homepage">
+            <h1>Welcome to the Home</h1>
+        </div>
+    )
+}
+export default Home
